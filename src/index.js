acff4f8afa1a96bad0dcb234cb22b22c8640e3a1
moroz <wertixvost@gmail.com>
2024-04-06 23:06:41 +0200

    remove attribution

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import {
     onCLS,
     onFID,
     onLCP,
-} from 'https://unpkg.com/web-vitals@3/dist/web-vitals.attribution.js?module';
+} from 'https://unpkg.com/web-vitals@3/dist/web-vitals.js?module';
 
 onCLS(console.log);
 onFID(console.log);
