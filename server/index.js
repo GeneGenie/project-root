1d508ad489dce97072479940e4f73362c2d50722
moroz <wertixvost@gmail.com>
2024-04-07 00:22:56 +0200

    step 5 receiving data

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = new express();
 
-
-app.post('/analytics', (req, res) => {
+app.use(cors());
+app.post('/analytics', express.json(), express.text(), (req, res) => {
+    if (typeof req.body === "string") req.body = JSON.parse(req.body);
     console.log('BE', req.body);
 
     res.sendStatus(204)
 })
+console.log('BE', 'listening');
 app.listen(3001)
