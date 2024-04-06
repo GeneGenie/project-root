3131d3ba5a534da7e0bfce1ed669a1e32a5d171b
moroz <wertixvost@gmail.com>
2024-04-07 01:07:20 +0200

    intorduce name

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,17 @@
 import nodeResolve from "@rollup/plugin-node-resolve";
 import dotenv from "rollup-plugin-dotenv";
+import clear from 'rollup-plugin-clear'
+import packageJSON from './package.json' assert {type: "json"};
 
-
+console.log(packageJSON)
 export default {
     input: 'src/index.js',
     output: {
-        file: 'public/dist/bundle.js',
+        file: `public/dist/${packageJSON.name}.js`,
         format: 'iife'
     },
     plugins: [
+        clear({targets:['public/dist']}),
         nodeResolve(),
         dotenv(),
     ]
