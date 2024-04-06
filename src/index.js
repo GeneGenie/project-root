86337debfdd70538d2193860181caf4f749e090f
moroz <wertixvost@gmail.com>
2024-04-06 23:58:41 +0200

    step 3 send to analytics

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import {onLCP, onFID, onCLS} from 'web-vitals';
 
-onCLS(console.log);
-onFID(console.log);
-onLCP(console.log);
+const opts = {reportAllChanges: true};
+
+function sendToAnalytics(metric) {
+    // Replace with whatever serialization method you prefer.
+    // Note: JSON.stringify will likely include more data than you need.
+    const body = JSON.stringify(metric);
+
+    // Use `navigator.sendBeacon()` if available, falling back to `fetch()`.
+    (navigator.sendBeacon && navigator.sendBeacon('/analytics', body)) ||
+    fetch('/analytics', {body, method: 'POST', keepalive: true});
+}
+function wvEventCallback(metric){
+    console.log(metric)
+    sendToAnalytics(metric)
+}
+
+onCLS(wvEventCallback, opts);
+onFID(wvEventCallback, opts);
+onLCP(wvEventCallback, opts);
