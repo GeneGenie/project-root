a56a1b18cd32b9e82a52e3571733aa85d07c42ca
moroz <wertixvost@gmail.com>
2024-04-06 22:46:39 +0200

    demo page

---
+++ notes/Requirements.md
@@ -0,0 +1,10 @@
+onCLS(console.log);
+onFID(console.log);
+onLCP(console.log);
+
+should be called once 
+
+callbacks may be triggered multiple times , and we should consider it 
+
+
+
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import {
+    onCLS,
+    onFID,
+    onLCP,
+} from 'https://unpkg.com/web-vitals@3/dist/web-vitals.attribution.js?module';
+
+onCLS(console.log);
+onFID(console.log);
+onLCP(console.log);
