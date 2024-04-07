eb5c059d976113a892791c3bb474f3d8a669f7cc
moroz <wertixvost@gmail.com>
2024-04-07 17:02:56 +0200

    заметки по логгеру

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,4 +1,8 @@
 export function Log(...args) {
+    // discussion about building in logger
+    // discussion about localstorage performance
+    // discussuon about ree shaking removal and lazy loading lib itself until required
+    // todo   considering possible performance issue -> develop lazy LS poller.
     if (localStorage.libdebug === '1') {
         console.log(...args)
     }
