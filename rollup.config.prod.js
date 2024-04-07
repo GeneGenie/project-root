5bcfff8015028add30911ccf0468e40ff8e475ed
moroz <wertixvost@gmail.com>
2024-04-07 02:11:46 +0200

    Visualize dependencies

--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -1,11 +1,17 @@
 import rollupConfig from './rollup.config.js'
 import terser from "@rollup/plugin-terser";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default {
     ...rollupConfig,
     plugins: [
         ...rollupConfig.plugins,
         terser(),
-
+        visualizer({
+            open:true,
+            template: 'treemap',
+            filename: 'public/dist/stats.html',
+            gzipSize:true
+        })
     ]
 }
