e6009e5e237a21a40428bb94b86b1a6ed21e8b33
moroz <wertixvost@gmail.com>
2024-04-08 22:53:38 +0200

    fix serve prod

--- build/serve.prod.js
+++ build/serve.prod.js
@@ -1,12 +1,10 @@
-import rollupConfig from './rollup.config.js'
-import terser from "@rollup/plugin-terser";
+import rollupConfig from './build.js'
 import serve from "rollup-plugin-serve";
 
 export default {
     ...rollupConfig,
     plugins: [
         ...rollupConfig.plugins,
-        terser(),
         serve({
             port: 3000,
             contentBase: ['public', 'public/dist'],
