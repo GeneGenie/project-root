0a180353e353a6121b53c30b9276edcfa4399ad4
moroz <wertixvost@gmail.com>
2024-04-09 00:59:12 +0200

    Protect build by removing source map
    PUT NOTES on using manual sourcemaps
    
    AND REPORT STILL WORKS

--- build/build.js
+++ build/build.js
@@ -1,10 +1,24 @@
 import rollupConfig from './rollup.config.js'
 import terser from "@rollup/plugin-terser";
 
+function removeSourcemapUrl() {
+    return {
+        name: 'remove-sourcemap-url',
+        generateBundle(options, bundle) {
+            for (const fileName in bundle) {
+                if (fileName.endsWith('.js')) {
+                    const chunk = bundle[fileName];
+                    chunk.code = chunk.code.replace(/\/\/# sourceMappingURL=.+\n?$/, '');
+                }
+            }
+        }
+    };
+}
 export default {
     ...rollupConfig,
     plugins: [
         ...rollupConfig.plugins,
         terser(),
+        removeSourcemapUrl()
     ]
 }
