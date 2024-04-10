305f10b794b116fde78bd8ecbf5a7f9c441b0c5c
moroz <wertixvost@gmail.com>
2024-04-10 19:34:40 +0200

    Cleanuo and refactor

---
+++ build/plugins/bundleSize.js
@@ -0,0 +1,21 @@
+import path from "path";
+import { gzipSizeSync } from "gzip-size";
+import fs from "fs";
+
+export function bundleSize() {
+    return {
+        name: 'bundle-size',
+        generateBundle(options, bundle) {
+            const asset = path.basename(options.file);
+            const code = bundle[asset].code;
+            const gzipped = gzipSizeSync(code)
+
+            fs.writeFileSync('reports/size.json', JSON.stringify({
+                [asset]: {
+                    size: code.length,
+                    gzipped
+                }
+            }))
+        }
+    }
+}
--- build/report.js
+++ build/report.js
@@ -1,32 +1,14 @@
 import rollupConfig from './build.js'
 import { visualizer } from "rollup-plugin-visualizer";
-import path from "path";
-import fs from "fs";
-import prettyBytes from 'pretty-bytes';
-import { gzipSizeSync } from 'gzip-size';
+import { bundleSize } from "./plugins/bundleSize.js";
 
 
 
-const format = size => prettyBytes(size);
 export default {
     ...rollupConfig,
     plugins: [
         ...rollupConfig.plugins,
-        {
-            name: 'bundle-size',
-            generateBundle(options, bundle) {
-                const asset = path.basename(options.file);
-                const code = bundle[asset].code;
-                const gzipped = gzipSizeSync(code)
-
-                fs.writeFileSync('reports/size.json', JSON.stringify({
-                    [asset]: {
-                        size: code.length,
-                        gzipped
-                    }
-                }))
-            }
-        },
+        bundleSize(),
         visualizer({
             sourcemap: true,
             template: 'treemap',
