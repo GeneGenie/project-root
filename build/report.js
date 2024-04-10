e6eb39ecfde5f3f911ff62b5104880d5e6498b0b
moroz <wertixvost@gmail.com>
2024-04-10 19:30:24 +0200

    Reporting bundle size  to json and host it in repo for simpliness

--- build/report.js
+++ build/report.js
@@ -1,15 +1,37 @@
 import rollupConfig from './build.js'
 import { visualizer } from "rollup-plugin-visualizer";
+import path from "path";
+import fs from "fs";
+import prettyBytes from 'pretty-bytes';
+import { gzipSizeSync } from 'gzip-size';
 
+
+
+const format = size => prettyBytes(size);
 export default {
     ...rollupConfig,
     plugins: [
         ...rollupConfig.plugins,
+        {
+            name: 'bundle-size',
+            generateBundle(options, bundle) {
+                const asset = path.basename(options.file);
+                const code = bundle[asset].code;
+                const gzipped = gzipSizeSync(code)
+
+                fs.writeFileSync('reports/size.json', JSON.stringify({
+                    [asset]: {
+                        size: code.length,
+                        gzipped
+                    }
+                }))
+            }
+        },
         visualizer({
-            sourcemap:true,
+            sourcemap: true,
             template: 'treemap',
             filename: 'reports/size.html',
-            gzipSize:true
+            gzipSize: true
         }),
     ]
 }
