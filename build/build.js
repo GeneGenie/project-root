46d8df5498408cd80c70162839caba48b28e931a
moroz <wertixvost@gmail.com>
2024-05-01 21:39:30 +0200

    split entrypoints

--- build/build.js
+++ build/build.js
@@ -1,6 +1,6 @@
 import rollupConfigs from './rollup.config.js';
 import terser from '@rollup/plugin-terser';
-import { removeSourcemapUrl } from './plugins/removeSourceMapUrl.js';
+// import { removeSourcemapUrl } from './plugins/removeSourceMapUrl.js';
 
 export default rollupConfigs.map((rollupConfig) => {
     return {
@@ -8,9 +8,8 @@ export default rollupConfigs.map((rollupConfig) => {
         output: [
             ...rollupConfig.output,
             {
+                ...rollupConfig.output[0],
                 file: rollupConfig.output[0].file.replace('.js', '.min.js'),
-                format: 'iife',
-                sourcemap: 'file',
                 plugins: [terser()],
             },
         ],
