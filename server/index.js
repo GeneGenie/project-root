ae700479ea3ebb287b00c902078307fd42fc0dd5
moroz <wertixvost@gmail.com>
2024-05-01 16:04:00 +0200

    server prod

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 
 const app = new express();
 
+app.use(express.static(path.join(process.cwd(), 'public')));
+app.use(express.static(path.join(process.cwd(), 'public/dist')));
 app.use(cors());
+
 app.post('/analytics', express.json(), express.text(), (req, res) => {
-    if (typeof req.body === "string") req.body = JSON.parse(req.body);
+    if (typeof req.body === 'string') req.body = JSON.parse(req.body);
     // console.log('BE', req.body);
 
-    res.sendStatus(204)
-})
+    res.sendStatus(204);
+});
 console.log('BE', 'listening');
-app.listen(3001)
+app.listen(80);
