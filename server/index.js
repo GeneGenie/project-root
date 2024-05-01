f04814e22a203a35dcaea8e0604e1dcb3ac9668b
moroz <wertixvost@gmail.com>
2024-05-01 22:15:01 +0200

    Whitelist sourcemaps

--- server/index.js
+++ server/index.js
@@ -2,9 +2,22 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 
+const whiteListedIps = ['84.123.85.38'];
 const app = new express();
 
 app.use(express.static(path.join(process.cwd(), 'public')));
+
+// middleware to allow *.js.map files to be served only for ip 127.0.0.1
+app.use((req, res, next) => {
+    const ip = req.socket.remoteAddress.split(':').pop();
+    console.log(ip);
+    if (req.path.endsWith('.js.map')) {
+        if (!whiteListedIps.includes(ip)) {
+            res.status(404).send('Not Found');
+        }
+    }
+    next();
+});
 app.use(express.static(path.join(process.cwd(), 'public/dist')));
 app.use(cors());
 
