a1dc3024bfceef3d87aebe4ccb0c1e1e53b1536e
moroz <wertixvost@gmail.com>
2024-05-03 13:52:04 +0200

    FAIL labeled logging

--- src/entrypoints/test.js
+++ src/entrypoints/test.js
@@ -1,6 +1,7 @@
-import { getLogger, Log } from 'logger';
+import { getLogger, Log } from '../lib/logger.js';
 const logger = getLogger('test');
 const log = Log.bind('[test]');
 APP_LOGS && Log(222);
 APP_LOGS && logger.log(333);
-APP_LOGS && log(444);
+// eslint-disable-next-line
+LOG: APP_LOGS && log(444);
