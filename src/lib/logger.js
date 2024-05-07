d82805a9fe58b2b59ab55bd6c80614b92b4a4b2e
moroz <wertixvost@gmail.com>
2024-05-07 19:08:19 +0200

    Some deploy polishing

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,28 +1,15 @@
 // this starts from capital only to avoid IDE auto fillilng as console.log
-
-export function Log(...args) {
-    // after tree shaking, all logs get removed from prod build
-
-    // discussion about building in logger
-    // discussion about localstorage performance
-    // discussuon about ree shaking removal and lazy loading lib itself until required
-    // todo   considering possible performance issue -> develop lazy LS poller.
-    // https://codesandbox.io/p/sandbox/localstoragevcookies-6w39h?file=%2Fsrc%2Ftest.ts%3A71%2C1
-    if (localStorage.libdebug === '1') {
-        console.log(...args);
-        // we could utilize trace here.
-    }
+function logAllowed() {
+    return localStorage.libdebug === '1';
 }
-
+export const Log = console.log.bind(console);
 export function getLogger(name) {
     // now much better
     // look at this minified code, it doesmnt get shaked away;
     // and this also doesn't help,
     return {
-        log(...args) {
-            Log(`[${name}]`, ...args);
-        },
-        // todo get here later
+        log: logAllowed() ? console.log.bind(console, `[${name}]`) : () => {},
+        // todo get here later++++
         // log: console.log.bind(console, `[${name}]`) // this is working but without localstorage ofcourse
     };
 }
